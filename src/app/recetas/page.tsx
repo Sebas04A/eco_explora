99bd1fe0c058ee529c57b4d4a9ba1f69a52fa3f0
Samaeeeel <asrovalino@puce.edu.ec>
2025-07-02 16:31:52 -0500

    Mejoras en los estilos de receta y cambios en la API de generación

--- src/app/recetas/page.tsx
+++ src/app/recetas/page.tsx
@@ -1,31 +1,50 @@
-import React from 'react'
+'use client';
+import React, { useState } from 'react'
 import { getRecetas } from '../api/recetas'
 import { Receta } from '../types/types'
 import ListaReceta from './ListaReceta'
+import GeneradorReceta from '../ui/GeneradorRecetas'
 
-async function Page() {
-    const recetas: Receta[] = await getRecetas()
+function Page() {
+  const [recetas, setRecetas] = useState<Receta[]>([]);
 
-    const secciones: { nombre: string; recetas: Receta[] }[] = []
-    for (const receta of recetas) {
-        const seccion = secciones.find(s => s.nombre === receta.Nombre) // Cambiar a Categoria si es necesario
-        if (seccion) {
-            seccion.recetas.push(receta)
-        } else {
-            secciones.push({ nombre: receta.Nombre, recetas: [receta] }) // Cambiar a Categoria si es necesario
-        }
+  // Aquí cargamos las recetas cuando el componente se monta
+  React.useEffect(() => {
+    const fetchRecetas = async () => {
+      const data = await getRecetas();
+      setRecetas(data);
     }
+    fetchRecetas();
+  }, []);  // El array vacío asegura que se ejecute solo una vez al cargar el componente
 
-    console.log('Secciones de recetas:', secciones)
-    return (
-        <>
-            <section className='welcome welcome-recetas'>
-                <h1>Recetas Naturales</h1>
-            </section>
+  const secciones: { nombre: string; recetas: Receta[] }[] = [];
+  for (const receta of recetas) {
+    const seccion = secciones.find(s => s.nombre === receta.Nombre);
+    if (seccion) {
+      seccion.recetas.push(receta);
+    } else {
+      secciones.push({ nombre: receta.Nombre, recetas: [receta] });
+    }
+  }
 
-            <ListaReceta recetas={recetas} />
-        </>
-    )
+  console.log('Secciones de recetas:', secciones);
+
+  // Aquí podemos tener un estado con las plantas seleccionadas
+  const [ingredientes, setIngredientes] = useState("tomate, albahaca, cebolla");
+
+  return (
+    <>
+      <section className='welcome welcome-recetas'>
+        <h1>Recetas Naturales</h1>
+      </section>
+
+      {/* Aquí se pasa la lista de ingredientes al generador */}
+      <GeneradorReceta ingredientes={ingredientes} />
+
+      {/* Aquí se listan las recetas disponibles */}
+      <ListaReceta recetas={recetas} />
+    </>
+  )
 }
 
 export default Page
