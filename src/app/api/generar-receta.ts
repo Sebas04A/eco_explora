6c24f87380ec2ef2b5fb2abd4caed509e6f1a76d
Samaeeeel <asrovalino@puce.edu.ec>
2025-07-03 08:51:57 -0500

    api bien

--- src/app/api/generar-receta.ts
+++ src/app/api/generar-receta.ts
@@ -1,5 +1,5 @@
 export async function generarReceta(ingredientes: string) {
-  const res = await fetch("https://api-f9z3.onrender.com", {
+  const res = await fetch("https://api-f9z3.onrender.com/receta", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ ingredientes }),
