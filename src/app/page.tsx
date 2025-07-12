79c44ebac83e6075153a050729666e5475220c01
ANDRE ARCENTALES <97565161+Sebas04A@users.noreply.github.com>
2025-07-12 18:41:47 -0500

    Add language switcher with Kichwa support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
-import Buscador from './ui/Buscador'
+'use client'
 
-export default async function Page() {
+import Buscador from './ui/Buscador'
+import { useContext } from 'react'
+import { LanguageContext } from './contexts/LanguageContext'
+
+export default function Page() {
+    const { t } = useContext(LanguageContext)
     // console.log('API URL:', process.env.NEXT_PUBLIC_API_URL)
     // let cargado = false
     // const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/planta`, {
@@ -19,12 +24,12 @@ export default async function Page() {
         <>
             <section className='welcome '>
                 <h1 className='text-shadow text-4xl font-bold text-green-800 px-4 py-6 rounded-xl text-center'>
-                    Bienvenido a EcoExplora
+                    {t('welcomeEcoExplora')}
                 </h1>
             </section>
             <section className='intro-ecoexplora px-6 py-8 bg-green-50 text-center max'>
                 <div className='max-w-3xl mx-auto'>
-                    <h2 className='text-2xl text-green-700 mb-4'>¿Qué es EcoExplora?</h2>
+                    <h2 className='text-2xl text-green-700 mb-4'>{t('whatIsEcoExplora')}</h2>
                     <p className='text-lg leading-relaxed text-gray-700'>
                         <strong>EcoExplora</strong> es un repositorio digital de plantas
                         clasificadas en categorías como medicinales, ornamentales, frutales y
